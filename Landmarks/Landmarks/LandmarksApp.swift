3deeef794392c52c9f3224325df132ee220b0fed
NORIKIM <jinagold.k@gmail.com>
2023-12-26 19:04:14 +0900

    프로젝트 생성

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김지나 on 2023/12/26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
