9cea22781292b304c6bc7cda9a6dc1ced8e50916
NORIKIM <jinagold.k@gmail.com>
2024-02-06 10:43:08 +0900

    section2. Create the row view

---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,26 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by 인터웍스_맥북 on 2/6/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
